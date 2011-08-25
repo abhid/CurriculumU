b3b2fe79655251326894fdd2a1f79e41e4d91281
Abhi Devireddy <abhi@devireddy.com>
2011-08-25 16:04:51 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curriculumu::Application.config.secret_token = 'c8d79df8f5ad537611e8bb100033a352debc30978ecf9e4b3c401bc38d3cf6bc488f0fdf047a7af65eb8c9416f7ff095880c84a5b0e92010dcc9bb2e4f210f2b'
